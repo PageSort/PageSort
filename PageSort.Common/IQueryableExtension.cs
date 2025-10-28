bb4ffe0cb20f032309f4cf6d6a1cad945c65406c
John Tuza <johntuza94@gmail.com>
2025-10-28 11:14:02 +0300

    async and dotnet 8 support

---
+++ PageSort.Common/IQueryableExtension.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+
+namespace PageSort.Common;
+
+public static class IQueryableExtension
+{
+    private static readonly MethodInfo OrderByMethod = typeof(Queryable)
+        .GetMethods()
+        .Where(method => method.Name == Constants.Ordering.OrderBy)
+        .Single(method => method.GetParameters().Length == 2);
+
+    private static readonly MethodInfo OrderByDescendingMethod = typeof(Queryable)
+        .GetMethods()
+        .Where(method => method.Name == Constants.Ordering.OrderByDescending)
+        .Single(method => method.GetParameters().Length == 2);
+
+    /// <summary>
+    /// Returns page collection of the specified source query.
+    /// </summary>
+    /// <param name="source">Source query</param>
+    /// <param name="pageNumber">Page number</param>
+    /// <param name="pageSize">Page size</param>
+    /// <returns>Ordered collection</returns>
+    public static IQueryable<T> Page<T>(this IQueryable<T> source, int pageNumber, int pageSize)
+    {
+        if (pageNumber <= 0)
+        {
+            throw new ArgumentException("The value provided can neither be a zero or a negative", nameof(pageNumber));
+        }
+
+        if (pageSize < 0)
+        {
+            throw new ArgumentException("The value provided can not be a negative", nameof(pageSize));
+        }
+
+        return source.Skip((pageNumber - 1) * pageSize)
+                     .Take(pageSize);
+    }
+
+    /// <summary>
+    /// Returns source ordered by the specified property and sort Direction.
+    /// </summary>
+    /// <param name="source">Source query</param>
+    /// <param name="propertyName">Property name</param>
+    /// <param name="sortDirection">Sort Direction</param>
+    /// <returns>Ordered collection</returns>
+    public static IQueryable<TSource> OrderByProperty<TSource>
+        (this IQueryable<TSource> source, string propertyName, ListSortDirection? sortDirection = ListSortDirection.Ascending)
+    {
+        var orderByProperty = GetOrderByExpression<TSource>(propertyName, out LambdaExpression lambda);
+
+        return sortDirection switch
+        {
+            ListSortDirection.Descending => 
+                GetSortedSource(source, OrderByDescendingMethod.MakeGenericMethod(typeof(TSource), orderByProperty.Type), lambda),
+
+            _ => GetSortedSource(source, OrderByMethod.MakeGenericMethod(typeof(TSource), orderByProperty.Type), lambda)
+        };
+
+    }
+
+
+    private static IQueryable<TSource> GetSortedSource<TSource>(IQueryable<TSource> source, MethodInfo genericMethod, LambdaExpression lambda)
+    {
+        object ret = genericMethod?.Invoke(null, [source, lambda])
+            ?? throw new ArgumentNullException(nameof(genericMethod));
+
+        return (IQueryable<TSource>)ret;
+    }
+
+    private static Expression GetOrderByExpression<TSource>(string propertyName, out LambdaExpression lambda)
+    {
+        ParameterExpression parameter = Expression.Parameter(typeof(TSource), "posting");
+        Expression orderByProperty = Expression.Property(parameter, propertyName);
+
+        lambda = Expression.Lambda(orderByProperty, parameter);
+        return orderByProperty;
+    }
+}
---
+++ PageSort.Common/PagingHelper.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace PageSort.Common;
+
+/// <summary>
+/// Paging helper. 
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public static class Page<T>
+{
+    /// <summary>
+    /// Pages and sorts the collection using pageQuery values.
+    /// </summary>
+    /// <param name="collection">Collection to be paged</param>
+    /// <param name="pageQuery">PageQuery that has values to be used during paging.</param>
+    /// <returns>PagedResult</returns>
+    public static PagedResult<T> GeneratePaging(IQueryable<T> collection, PageQuery pageQuery)
+    {
+        ArgumentNullException.ThrowIfNull(pageQuery);
+        ArgumentNullException.ThrowIfNull(collection);
+
+        int count = collection.Count();
+        int CurrentPage = pageQuery.PageNumber;
+        int PageSize = pageQuery.PageSize;
+        int TotalCount = count;
+        int TotalPages = (int)Math.Ceiling(count / (double)PageSize);
+
+        if (pageQuery.SortProperty is not null)
+            collection = collection.OrderByProperty(pageQuery.SortProperty, pageQuery.SortDirection ?? ListSortDirection.Ascending);
+
+        return new PagedResult<T>
+        {
+            CurrentPage = CurrentPage,
+            NextPage = CurrentPage < TotalPages,
+            PageSize = PageSize,
+            PreviousPage = CurrentPage > 1,
+            Collection = [.. collection.Page(CurrentPage, PageSize)],
+            TotalCount = TotalCount,
+            TotalPages = TotalPages
+        };
+    }
+
+    public static Task<PagedResult<T>> GeneratePagingAsync(IQueryable<T> collection, PageQuery pageQuery)
+    {
+        ArgumentNullException.ThrowIfNull(collection);
+        ArgumentNullException.ThrowIfNull(pageQuery);
+
+        int count = collection.Count();
+        int currentPage = pageQuery.PageNumber;
+        int pageSize = pageQuery.PageSize;
+        int totalCount = count;
+        int totalPages = (int)Math.Ceiling(count / (double)pageSize);
+
+        if (pageQuery.SortProperty is not null)
+            collection = collection.OrderByProperty(pageQuery.SortProperty, pageQuery.SortDirection ?? ListSortDirection.Ascending);
+
+
+        return Task.FromResult(new PagedResult<T>
+        {
+            CurrentPage = currentPage,
+            NextPage = currentPage < totalPages,
+            PageSize = pageSize,
+            PreviousPage = currentPage > 1,
+            Collection = [.. collection.Page(currentPage, pageSize)],
+            TotalCount = totalCount,
+            TotalPages = totalPages
+        });
+
+    }
+}
